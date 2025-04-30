9768cdb0b7800cdb8bf0fe67543d0af540133adf
bharatheekota <Bharathi_Kota@external.mckinsey.com>
2025-04-30 15:47:00 -0500

    Update provider.tf

--- modules/cognito-auth/provider.tf
+++ modules/cognito-auth/provider.tf
@@ -1,15 +1,11 @@
 terraform {
-  required_version = "1.3.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-east-1"     
-  profile = "default"        
+  region  = "us-east-1"          
 }
